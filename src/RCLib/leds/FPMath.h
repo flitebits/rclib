20cc0d094633c99f0d5536dc604a8028356cb884
flitetbits <flitebits@gmail.com>
2022-08-07 15:45:25 -0400

    Provide a standard version of Logify, provide a reusable version of 'VariableSaw'. Make LedSpan a library class.

--- src/RCLib/leds/FPMath.h
+++ src/RCLib/leds/FPMath.h
@@ -1,9 +1,9 @@
 // Copyright 2020,2021 Thomas DeWeese
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 
 #ifndef _LED_FPMATH_
@@ -18,13 +18,13 @@ namespace led {
 // Takes 3-4 cycles
 inline u8_t scale8(u8_t v0, u8_t v1) {
   u8_t out;
-  __asm__ __volatile__ (		     \
-			"mul %[v0], %[v1]\n\t" \
-			"mov %[out], r1\n\t"   \
-			"clr __zero_reg__  \n\t" \
-			: [out] "=r" (out)
-			: [v0] "r" (v0),
-			  [v1] "r" (v1));
+  __asm__ __volatile__ (                     \
+                        "mul %[v0], %[v1]\n\t" \
+                        "mov %[out], r1\n\t"   \
+                        "clr __zero_reg__  \n\t" \
+                        : [out] "=r" (out)
+                        : [v0] "r" (v0),
+                          [v1] "r" (v1));
   return out;
 }
 
@@ -35,47 +35,47 @@ inline u8_t scale8(u8_t v0, u8_t v1) {
 // Takes 5-6 cycles.
 inline u8_t bscale8(u8_t v0, u8_t v1) {
   u8_t out;
-  __asm__ __volatile__ (		     \
-			"mul %[v0], %[v1]\n\t" \
-			"mov %[out], r1\n\t"   \
-			"clr __zero_reg__  \n\t" \
-			"add r0, %[v1]\n\t" \
-			"adc %[out], __zero_reg__\n\t" \
-			: [out] "=r" (out)
-			: [v0] "r" (v0),
-			  [v1] "r" (v1));
+  __asm__ __volatile__ (                     \
+                        "mul %[v0], %[v1]\n\t" \
+                        "mov %[out], r1\n\t"   \
+                        "clr __zero_reg__  \n\t" \
+                        "add r0, %[v1]\n\t" \
+                        "adc %[out], __zero_reg__\n\t" \
+                        : [out] "=r" (out)
+                        : [v0] "r" (v0),
+                          [v1] "r" (v1));
   return out;
 }
-  
+
 inline u16_t scale16(u16_t v0, u16_t v1) {
   u16_t out;
-  __asm__ __volatile__ (		       \
-			/* multiply high bytes and make output word */ \
-			"mul %B1, %B2    \n\t" \
-			"movw %[out], r0 \n\t" \
-			/* multiply low bytes only keep high byte for */ \
-			/* accumulation purposes to catch carries */ \
-			"mul %A1, %A2    \n\t" \
-			"mov r24, r1     \n\t" \
-			/* multiply hi-lo bytes, add low byte to */ \
-			/* accumulator byte and high byte to low byte of */ \
-			/* result, with carry, the add any carry from that */ \
-			/* to the high result byte */ \
-			"mul %B1, %A2    \n\t" \
-			"clr r25         \n\t" \
-			"add r24, r0     \n\t" \
-			"adc %A0, r1     \n\t" \
-			"adc %B0, r25    \n\t" \
-			/* repeat with lo-hi result */
-			"mul %A1, %B2    \n\t" \
-			"add r24, r0     \n\t" \
-			"adc %A0, r1     \n\t" \
-			"adc %B0, r25    \n\t" \
-			/* clear zero reg that we used */ \
-			"clr __zero_reg__  \n\t" \
-			: [out] "=r" (out)
-			: "a" (v0),
-			  "a" (v1)
+  __asm__ __volatile__ (                       \
+                        /* multiply high bytes and make output word */ \
+                        "mul %B1, %B2    \n\t" \
+                        "movw %[out], r0 \n\t" \
+                        /* multiply low bytes only keep high byte for */ \
+                        /* accumulation purposes to catch carries */ \
+                        "mul %A1, %A2    \n\t" \
+                        "mov r24, r1     \n\t" \
+                        /* multiply hi-lo bytes, add low byte to */ \
+                        /* accumulator byte and high byte to low byte of */ \
+                        /* result, with carry, the add any carry from that */ \
+                        /* to the high result byte */ \
+                        "mul %B1, %A2    \n\t" \
+                        "clr r25         \n\t" \
+                        "add r24, r0     \n\t" \
+                        "adc %A0, r1     \n\t" \
+                        "adc %B0, r25    \n\t" \
+                        /* repeat with lo-hi result */
+                        "mul %A1, %B2    \n\t" \
+                        "add r24, r0     \n\t" \
+                        "adc %A0, r1     \n\t" \
+                        "adc %B0, r25    \n\t" \
+                        /* clear zero reg that we used */ \
+                        "clr __zero_reg__  \n\t" \
+                        : [out] "=r" (out)
+                        : "a" (v0),
+                          "a" (v1)
                         : "r24", "r25");
   return out;
 }
@@ -118,6 +118,52 @@ inline u16_t scale16(u16_t v0, u16_t v1) {
  // Returns a sin wave going from lo->hi with a frequency of
  // bmp88. Uses FastTimeNow to get current time, and 1 min = 64sec.
  i16_t bpm16Ranged(u16_t bpm88, i16_t lo, i16_t hi);
- 
+
+inline u8_t Logify(u8_t val) {
+  static const u8_t log_map[9] = {0, 1, 2, 5, 11, 24, 52, 115, 255};
+  u8_t lo = val & 0x1F;
+  u8_t hi = val >> 5;
+  int ret = ((u16_t(log_map[hi]) << 5) +
+             (i16_t(log_map[hi + 1]) - log_map[hi]) * lo);
+  return (ret + (1<<4)) >> 5;
+}
+
+// This supports changing the saw pattern speed while running without
+// introducing big jumps.  It basicaly aligns the phase of the new saw
+// with the previous saw at the time of the speed change.
+// Speed is the counts/ms as a 8.8 Fixed Point Number.
+// If you set it to 1 then it completes a cycle in ~1 minute
+class VariableSaw {
+public:
+  VariableSaw(u16_t speed = 0) : off_(0), spd_(speed), prev_time_(0) { }
+
+  // Speed is the counts/ms as a 8.8 Fixed Point Number.
+  void SetSpeed(u16_t speed) {
+    u8_t prev = Get(prev_time_);
+    spd_ = speed;
+    u8_t curr = Get(prev_time_);
+    // Add the difference to off so that at prev_time_ the old speed and
+    // new speed have the same value.
+    off_ += prev - curr;
+  }
+
+  u8_t Get(u16_t now) {
+    prev_time_ = now;
+    return u8_t(((now * spd_) + (1 << 7)) >> 8) + off_;
+  }
+
+  u8_t GetOff() { return off_; }
+  void SetState(u16_t speed, u8_t off) {
+    spd_ = speed;
+    off_ = off;
+  }
+
+
+private:
+  u8_t off_;
+  u16_t spd_;
+  u16_t prev_time_;
+};
+
 }  // namespace led
 #endif  // _LED_FPMATH_
---
+++ src/RCLib/leds/LedSpan.h
@@ -0,0 +1,112 @@
+// Copyright 2022 Thomas DeWeese
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+#include <stdlib.h>
+#include "IntTypes.h"
+
+// Provides a convenient way to reference a run of pixels out of a larger string
+// of leds.  This is nice for situations where you have logically discreate
+// groups of leds that are all part of one longer serial chain of leds.
+template <class P>
+class LedSpan {
+public:
+  // 'leds' should point to the first byte of the first pixel.
+  // 'len' is the length of the led run in pixels (not bytes).
+  // If reverse is true then all indexing is reversed (At(0) returns
+  // leds[len-1], etc).
+  LedSpan(void* leds = NULL, u8_t len = 0, bool reverse = false) :
+    ptr_(reinterpret_cast<P*>(leds)), len_(len), reverse_(reverse) { }
+
+  // Can be used to initialize a Span after construction.
+  // This also has the nice feature of returning a pointer to the
+  // pixel immediately after this span.
+  // This makes code like this possible:
+  // void *ptr = led_array;
+  // ptr = left_span.SetSpan(ptr, n);
+  // ptr = right_span.SetSpan(ptr, m);
+  // ...
+  void* SetSpan(void* leds, u8_t len, bool reverse = false) {
+    ptr_ = reinterpret_cast<P*>(leds);
+    len_ = len;
+    reverse_ = reverse;
+    return ptr_ + len_;
+  }
+
+  // Returns the length of this span.
+  u8_t len() const { return len_; }
+  // Returns a ptr to the start of the pixel after this span (see SetSpan
+  // for more info).
+  void* next_ptr() const { return ptr_ + len_; }
+
+  // Return a reference to the requested pixel (taking reverse into account).
+  P& At(u8_t idx) {
+    if (idx < 0) idx = 0;
+    else if ( idx > len_ - 1) idx = len_ - 1;
+
+    if (reverse_) {
+      return ptr_[len_ - 1 - idx];
+    }
+    return ptr_[idx];
+  }
+  // Return a reference to the requested pixel (taking reverse into account).
+  const P& At(u8_t idx) const {
+    if (idx < 0) idx = 0;
+    else if ( idx > len_ - 1) idx = len_ - 1;
+
+    if (reverse_) {
+      return ptr_[len_ - 1 - idx];
+    }
+    return ptr_[idx];
+  }
+  // Sets the specified pixel to the provided color.
+  void Set(u8_t idx, const P& pix) {
+        At(idx) = pix;
+  }
+  // Fills all the pixels in the span to to specified color.
+  void Fill(const P& pix) {
+    P* ptr = ptr_;
+    P* end = ptr_ + len_;
+    while (ptr != end) *(ptr++) = pix;
+  }
+
+  // Calls 'op' with 8bit fraction (0 -> index 0, 255 -> index len-1)
+  // and the associated pixel for every pixel in the span.
+  template <class O>
+  void FillOp(const O& op) {
+    u16_t step = (u16_t(255) << 8) / len_;
+    // Use step/2 so it points at the midpoint of the span the pixel covers.
+    u16_t frac = step >> 1;
+    for (u8_t i = 0; i < len_; ++i, frac += step) {
+      P& pix = At(i);
+      op(u8_t(frac >> 8), &pix);
+    }
+  }
+
+  // Moves all pixels one place (wraps last one back to start).
+  // forward true means N -> N+1, false means N+1 -> N (accounts for reverse_).
+  void Rotate(bool forward = true) {
+    if (forward == reverse_) {
+      P p0 = ptr_[0];
+      for (int i = 0; i < len_ - 1; ++i) {
+        ptr_[i] = ptr_[i + 1];
+      }
+      ptr_[len_ - 1] = p0;
+    } else {
+      P pn = ptr_[len_ - 1];
+      for (int i = len_ - 1; i > 0; --i) {
+        ptr_[i] = ptr_[i - 1];
+      }
+      ptr_[0] = pn;
+    }
+  }
+
+protected:
+  P* ptr_;
+  u8_t len_;
+  bool reverse_;
+};
